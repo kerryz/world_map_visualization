5816f923aadc1ef4742d27c6a9a3a6a00b426880
kerryz <kerryzhang92@gmail.com>
2015-05-13 17:28:45 +0800

    Azimuthal seems to be working even though lat and long order is wrong when reading landshape.txt ...

---
+++ src/azimuthal_equidistant.py
@@ -0,0 +1,66 @@
+from __future__ import division
+from math import radians, cos, sin, acos
+
+
+CENTER_COORD = (45.0, 0.0)  # (latitude, longtitude)
+
+
+def main():
+    azimuth = AzimuthalEquidistant(CENTER_COORD)
+    print azimuth.project((45.0, 10.0))
+
+
+class AzimuthalEquidistant(object):
+    """
+    Formulas taken from
+    http://mathworld.wolfram.com/AzimuthalEquidistantProjection.html
+    """
+    def __init__(self, (lat0, long0)):
+        """
+        Parameters
+        ----------
+        (lat0, long0) : (float, float)
+            latitude and longtitude coordinates of the center of the projections,
+            given as degrees, called phi_1 and lambda_0 in the Wolfram link
+        """
+        self.p1 = radians(lat0)  # phi_1
+        self.l0 = radians(long0)  # lambda_0
+        # optimization: storing these values because they are used
+        # when calculating c, the angular distance from the center
+        self.cos_p1 = cos(self.p1)
+        self.sin_p1 = sin(self.p1)
+
+    def project(self, (lat, long)):
+        # if same coordinates as center point, then c = 0
+        # and k = c / sin(c) will perform division by 0
+        if (lat, long) == (self.l0, self.p1):
+            return (0.0, 0.0)
+
+        # convert to radians and rename variables to match Wolfram link
+        p = radians(lat)
+        l = radians(long)
+        # optimization, store repeated calculations
+        sin_p = sin(p)
+        cos_p = cos(p)
+        cosp_cosll0 = cos_p * cos(l - self.l0)
+
+        # calculate c, the angular distance from the center
+        cos_c = self.sin_p1 * sin_p + self.cos_p1 * cosp_cosll0
+        c = acos(cos_c)
+        if c == 0:
+            return (0.0, 0.0)
+        # k'
+        try:
+            k = c / sin(c)
+        except ZeroDivisionError:
+            print lat, long
+
+        # calculate Azimuthal Equidistant projection x, y coordinates
+        x = k * cos_p * sin(l - self.l0)
+        y = k * (self.cos_p1 * sin_p - self.sin_p1 * cosp_cosll0)
+
+        return (x, y)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/map_projections.py
@@ -0,0 +1,42 @@
+from __future__ import division
+
+import matplotlib.pyplot as plt
+
+from azimuthal_equidistant import AzimuthalEquidistant
+
+
+LANDSHAPE_FILEPATH = "../data/landshape.txt"
+
+CENTER_COORDS = (0.0, 0.0)
+
+
+def main():
+    # get coordinates
+    landshape_coords = get_landshape_coords(LANDSHAPE_FILEPATH, lat_range=(-90, 90))
+
+    # Azimuth equidistant projection
+    azimuth = AzimuthalEquidistant(CENTER_COORDS)
+    azimuth_coords = [azimuth.project(coord) for coord in landshape_coords]
+    plt.plot([long for (lat, long) in azimuth_coords], [lat for (lat, long) in azimuth_coords], '.')
+    plt.show()
+
+
+def get_landshape_coords(landshape_filepath, lat_range=(-90, 90), long_range=(-90, 90)):
+    landshape_coords = []
+    with open(landshape_filepath) as file:
+        lat_min, lat_max = 0, 0
+        long_min, long_max = 0, 0
+        for line in file:
+            lat, long = line.strip().split(" ")
+            lat, long = float(lat), float(long)
+            lat_min, lat_max = min(lat_min, lat), max(lat_max, lat)
+            long_min, long_max = min(long_min, long), max(long_max, long)
+            if lat_range[0] <= lat <= lat_range[1]:
+                landshape_coords.append((lat, long))
+    print "lat range", (lat_min, lat_max)
+    print "long range", (long_min, long_max)
+    return landshape_coords
+
+
+if __name__ == "__main__":
+    main()
