898304bf85dbee55b4ba207269eb15a1fb663582
kerryz <kerryzhang92@gmail.com>
2015-05-13 18:42:22 +0800

    Azimuth projection pretty much done, nice black bg and white dots

--- src/map_projections.py
+++ src/map_projections.py
@@ -1,45 +1,51 @@
 from __future__ import division
 
 import matplotlib.pyplot as plt
+# from matplotlib.colors import hex2color
 
 from azimuthal_equidistant import AzimuthalEquidistant
 
 
 LANDSHAPE_FILEPATH = "../data/landshape.txt"
 
-CENTER_COORDS = (0.0, 0.0)
+CENTER_COORDS = (45, 0.0)  # latitude, longtitude
+LAT_RANGE = (10, 80)  # latitude range
 
 
 def main():
     # get coordinates
-    landshape_coords = get_landshape_coords(LANDSHAPE_FILEPATH, lat_range=(-90, 90))
+    landshape_coords = get_landshape_coords(LANDSHAPE_FILEPATH, lat_range=LAT_RANGE)
 
     # Azimuth equidistant projection
     azimuth = AzimuthalEquidistant(CENTER_COORDS)
     azimuth_coords = [azimuth.project(coord) for coord in landshape_coords]
-    plt.plot(
+    fig, ax = plt.subplots()
+    ax.set_title("Azimuth Equidistant Projection")
+    ax.plot(
         [x for (x, y) in azimuth_coords],
         [y for (x, y) in azimuth_coords],
-        '.', markersize=1.0
+        'w.', markersize=0.04
     )
+    # ax.set_axis_bgcolor((0.9686274509803922, 0.9450980392156862, 0.8745098039215686))
+    ax.set_axis_bgcolor('black')
     plt.show()
 
 
-def get_landshape_coords(landshape_filepath, lat_range=(-90, 90), long_range=(-90, 90)):
+def get_landshape_coords(landshape_filepath, lat_range=(-90, 90), long_range=(-180, 180)):
+    """
+    Returns
+    -------
+    landshape_coords :: [(float, float)]
+        [(latitude, longtitude)]
+    """
     landshape_coords = []
     with open(landshape_filepath) as file:
-        lat_min, lat_max = 0, 0
-        long_min, long_max = 0, 0
         for line in file:
             # landshape.txt stores coordinates in (longtitude, latitude) order
             long, lat = line.strip().split(" ")
             lat, long = float(lat), float(long)
-            lat_min, lat_max = min(lat_min, lat), max(lat_max, lat)
-            long_min, long_max = min(long_min, long), max(long_max, long)
-            if lat_range[0] <= lat <= lat_range[1]:
+            if lat_range[0] <= lat <= lat_range[1] and long_range[0] <= long <= long_range[1]:
                 landshape_coords.append((lat, long))
-    print "lat range", (lat_min, lat_max)
-    print "long range", (long_min, long_max)
     return landshape_coords
 
 
